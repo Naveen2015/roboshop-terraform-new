9a93af833fc4f5351df5b5da5bc9d7b524aa899e
naveenporeddy <naveenkumar9707@gmail.com>
2025-04-17 08:46:25 -0500

    terraform learning

--- servers.tf
+++ servers.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 data "aws_ami" "example" {
   most_recent = true
   owners = ["973714476881"]
@@ -6,4 +14,13 @@ data "aws_ami" "example" {
 
 output "data" {
   value = data.aws_ami.example.image_id
+}
+
+resource "aws_instance" "frontend" {
+  ami = data.aws_ami.example.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "frontend"
+  }
+
 }
