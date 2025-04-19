f59ee8e9a5034042a6ae4083dc02a176c8db34de
naveenporeddy <naveenkumar9707@gmail.com>
2025-04-18 19:04:18 -0500

    structuring of code

--- servers.tf
+++ servers.tf
@@ -1,61 +1,6 @@
-data "aws_ami" "example" {
-  most_recent = true
-  owners = ["973714476881"]
-  name_regex       = "Centos-8-DevOps-Practice"
-}
-
-
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
-
-variable "components" {
-  default = {
-    frontend = {
-      name = "frontend"
-      instance_type = "t3.micro"
-    }
-    mongodb = {
-      name = "mongodb"
-      instance_type = "t3.micro"
-    }
-    catalogue = {
-      name = "catalogue"
-      instance_type = "t3.micro"
-    }
-    user = {
-      name = "user"
-      instance_type = "t3.micro"
-    }
-    cart = {
-      name = "cart"
-      instance_type = "t3.micro"
-    }
-    mysql = {
-      name = "mysql"
-      instance_type = "t3.micro"
-    }
-    rabbitmq = {
-      name = "rabbitmq"
-      instance_type = "t3.micro"
-    }
-    redis = {
-      name = "redis"
-      instance_type = "t3.micro"
-    }
-    shipment = {
-      name = "shipment"
-      instance_type = "t3.small"
-    }
-    payment = {
-      name = "payment"
-      instance_type = "t3.micro"
-    }
-  }
-}
 resource "aws_instance" "instance" {
   for_each = var.components
-  ami = data.aws_ami.example.image_id
+  ami = data.aws_ami.ami.image_id
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
   tags = {
