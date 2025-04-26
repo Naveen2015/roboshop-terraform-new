215f07e2bc77059bd70cada9330aeb6025df3585
naveenporeddy <naveenkumar9707@gmail.com>
2025-04-26 11:54:09 -0500

    ansible pull

--- module/main.tf
+++ module/main.tf
@@ -11,7 +11,6 @@ resource "aws_instance" "instance" {
 }
 
 resource "null_resource" "provisioner" {
-  count = var.provisioner ? 1 : 0
   depends_on = [aws_instance.instance,aws_route53_record.records]
   provisioner "remote-exec" {
 
@@ -34,4 +33,28 @@ resource "aws_route53_record" "records" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
+}
+
+resource "aws_iam_role" "role" {
+  name = "${var.component_name}--${var.env}-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.component_name}--${var.env}-role"
+  }
 }
