308b2f27ae58cbdb4a7300cb4a9691bbfa20efe1
naveenporeddy <naveenkumar9707@gmail.com>
2025-04-26 12:00:45 -0500

    ansible pull

--- module/locals.tf
+++ module/locals.tf
@@ -10,6 +10,6 @@ locals {
   ]
   app_commands = [
   "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/Naveen2015/roboshop-ansible-new.git -e env=${var.env} roboshop.yml -e role_name = ${var.component_name}"
+    "ansible-pull -i localhost, -U https://github.com/Naveen2015/roboshop-ansible-new -e env=${var.env} roboshop.yml -e role_name = ${var.component_name}"
   ]
 }
