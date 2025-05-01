4a53446ff8d96f1d9565d8951f02be6081b879b4
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-01 12:02:36 -0500

    vpc

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git:https://github.com/Naveen2015/tf-module-vpc-new.git"
+  source = "git::https://github.com/Naveen2015/tf-module-vpc-new.git"
 
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
