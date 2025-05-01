ba924d2648d96d6c290271f3a92b92fa170bae93
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-01 12:49:03 -0500

    subnets

--- main.tf
+++ main.tf
@@ -3,10 +3,14 @@ module "vpc" {
 
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
-  tags = merge(local.tags, { Name = "${var.env}-vpc"})
+  subnets = each.value["subnets"]
+  tags = local.tags
+  env = var.env
 
 }
+
 
 output "vpc_main_module" {
   value = module.vpc
 }
+
