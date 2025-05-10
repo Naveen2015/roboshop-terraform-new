07d2bc5502c8a04f12ad2ccab6cab7db1bbd11d4
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-10 13:00:44 -0500

    auto scalling group

--- main.tf
+++ main.tf
@@ -14,22 +14,22 @@ output "vpc_main_module" {
   value = module.vpc
 }
 
-# module "app" {
-#   source   = "git::https://github.com/Naveen2015/tf-module-app-new.git"
-#   for_each = var.app
-#   instance_type = each.value["instance_type"]
-#   name = each.value["name"]
-#   env = var.env
-#
-#   desired_capacity = each.value["desired_capacity"]
-#   max_size = each.value["max_size"]
-#   min_size = each.value["min_size"]
-#   bastion_cidr = var.bastion_cidr
-#   subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids ",null)
-#   vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
-#   allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_app_cidr"],null),"subnet_cidrs",null)
-#
-#
-# }
+module "app" {
+  source   = "git::https://github.com/Naveen2015/tf-module-app-new.git"
+  for_each = var.app
+  instance_type = each.value["instance_type"]
+  name = each.value["name"]
+  env = var.env
+
+  desired_capacity = each.value["desired_capacity"]
+  max_size = each.value["max_size"]
+  min_size = each.value["min_size"]
+  bastion_cidr = var.bastion_cidr
+  subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null)
+  vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_app_cidr"],null),"subnet_cidrs",null)
+
+
+}
 
 
